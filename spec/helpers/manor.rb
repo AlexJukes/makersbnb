b476ce99a7f109926e22603a3ce1ac6cf71f9a52
AlexJukes <alexanderjukes@gmail.com>
2017-04-18 18:09:17 +0100

    Create manor class, begin writing manor/new.erb

--- spec/helpers/manor.rb
+++ spec/helpers/manor.rb
@@ -4,7 +4,7 @@ def create_manor
   fill_in :name, with: 'Buckingham Palace'
   fill_in :description, with: 'Lovely quaint 50 bedroom home, like new but with a historial touch. Please note: Guards are not included.'
   fill_in :price, with: 1.99
-  fill_in :availableFrom, with: Date
-  fill_in :availableTo, with: Date
+  fill_in :dateFrom, with: Date
+  fill_in :dateTo, with: Date
   click_button('Flog it')
 end
