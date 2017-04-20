a4c55c27675f300b75d14444029da73718783cf9
Colin Turner <colinturner34@gmail.com>
2017-04-20 17:05:56 +0100

    Connect models

--- spec/features/request_spec.rb
+++ spec/features/request_spec.rb
@@ -7,9 +7,12 @@ feature 'Request manor' do
   scenario 'I want to select a night to request a manor' do
     set_up_owner
     sign_out
-
     set_up_letter
     expect { make_request }.to change(Request, :count).by(1)
+  end
+
+  scenario '' do
+
   end
 
 end
