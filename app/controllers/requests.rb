fafd98b50fd25c92e25e5dacedfcea12ad167eaa
Edward Ward <edwardwardward@gmail.com>
2017-04-21 11:01:42 +0100

    small bug fix

--- app/controllers/requests.rb
+++ app/controllers/requests.rb
@@ -1,15 +1,15 @@
 class ExitBeforeBrexit < Sinatra::Base
 
   post '/requests/new' do
-
     if current_user
       current_user.requests.create(date_from: params[:reservation_arrival_date],
                                   manor: Manor.first(params[:manor_id]))
-      redirect '/requests
+      redirect '/requests'
     else
       flash.next[:notice] = 'You must be logged in to request a manor'
       redirect('/sessions/new')
     end
+  end
 
 
   get '/requests' do
